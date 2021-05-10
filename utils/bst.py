e00c062015a05ac4ecc0dbab3f8bf5c2ae6ac30f
unknown <prophreaker@gmail.com>
2021-05-11 00:26:24 +0300

    feat: add display for bst

--- utils/bst.py
+++ utils/bst.py
@@ -1,7 +1,10 @@
+from collections import deque
+
+
 class BSTNode:
-    def __init__(self, value=None):
-        self.left = None
-        self.right = None
+    def __init__(self, value=None, lc=None, rc=None):
+        self.left = lc
+        self.right = rc
         self.value = value
 
     def insert(self, value):
@@ -24,24 +27,52 @@ class BSTNode:
             return
         self.right = BSTNode(value)
 
-    def delete(self, value):
-        if self == None:
-            return self
-        if value < self.value:
-            if self.left:
-                self.left = self.left.delete(value)
-            return self
-        if value > self.value:
-            if self.right:
-                self.right = self.right.delete(value)
-            return self
-        if self.right == None:
-            return self.left
-        if self.left == None:
-            return self.right
-        min_larger_node = self.right
-        while min_larger_node.left:
-            min_larger_node = min_larger_node.left
-        self.value = min_larger_node.value
-        self.right = self.right.delete(min_larger_node.value)
-        return self
+    def display(self):
+        lines, *_ = self._display_aux()
+        for line in lines:
+            print(line)
+
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = '%s' % self.value
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % self.value
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % self.value
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = '%s' % self.value
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
