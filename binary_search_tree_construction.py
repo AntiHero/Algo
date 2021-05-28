7ef9494b5376874ec138f1e38bde69ea619d3515
Aleksandr Schemelev <prophreaker@gmail.com>
2021-05-28 22:50:57 +0300

    feat: add insert method to BST

--- binary_search_tree_construction.py
+++ binary_search_tree_construction.py
@@ -1,2 +1,24 @@
-def BST:
-    # TODO
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        current_node = self
+        while True:
+            if value < current_node.value:
+                if current_node.left is None:
+                    current_node.left = BST(value)
+                    break
+                else:
+                    current_node = current_node.left
+
+            else:
+                if current_node.right is None:
+                    current_node.right = BST(value)
+                    break
+                else:
+                    current_node = current_node.right
+
+        return self
