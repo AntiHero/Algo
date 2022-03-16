61b4df92f196527ca58781a6536c76c2f00eb789
Aleksandr Schemelev <prophreaker@gmail.com>
2022-03-16 23:40:38 +0300

    task: add invert binary tree solution

---
+++ invert_binary_tree.py
@@ -0,0 +1,45 @@
+from asyncio.windows_events import NULL
+from utils import bst
+
+root = bst.BSTNode(10)
+root.insert(5)
+root.insert(15)
+root.insert(5)
+root.insert(8)
+root.insert(6)
+root.insert(2)
+root.insert(1)
+root.insert(22)
+root.display()
+
+
+# O(N) time | O(d) time, wher d - depth | O(lon(N))
+def invert_binary_tree_recursively(tree):
+    if tree is None:
+        return
+    tree.right, tree.left = tree.left, tree.right
+    invert_binary_tree_recursively(tree.left)
+    invert_binary_tree_recursively(tree.right)
+
+
+invert_binary_tree_recursively(root)
+print('\n')
+root.display()
+
+
+# O(N) time | O(N) space
+def invert_binary_tree(tree):
+    queue = [tree]
+
+    while len(queue):
+        current = queue.pop(0)
+        if current is None:
+            continue
+        current.left, current.right = current.right, current.left
+        queue.append(current.left)
+        queue.append(current.right)
+
+
+invert_binary_tree(root)
+print('\n')
+root.display()
